271731ad0013c521e56265f776c2752cdbc5e5ca
Coutinho, Reuben <reuben.coutinho@accenture.com>
2017-09-16 21:03:32 +1000

    Freature: added script for camera follow

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform target;
+	public float smoothing = 5f;
+
+	Vector3 offest;
+
+	void Start(){
+		offest = transform.position - target.position;
+	}
+
+	void FixedUpdate(){
+		//To follow a phyisic object
+		//If we used Update() instead it would move in a different time
+
+		Vector3 targetCamPos = target.position + offest;
+		transform.position = Vector3.Lerp (transform.position, targetCamPos, smoothing * Time.deltaTime); 
+		//lerp helps smothly transition
+	}
+}
